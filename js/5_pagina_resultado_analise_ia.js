07835ba3e1a898b299e1a86b7c4adba2dcea2e5e
joelsonNS <joelson.n.s@hotmail.com>
2026-01-22 13:11:47 -0300

    Criação da página 5
    Criação da página 5, analise da imagem de momografia

---
+++ js/5_pagina_resultado_analise_ia.js
@@ -0,0 +1,13 @@
+// Botões de ação
+const editarBtn = document.querySelector(".btn.secondary");
+const validarBtn = document.querySelector(".btn.primary");
+
+// Evento para editar o laudo
+editarBtn.addEventListener("click", () => {
+    alert("Função de edição ativada (simulação).");
+});
+
+// Evento para validar o laudo
+validarBtn.addEventListener("click", () => {
+    alert("Laudo validado com sucesso! ✅");
+});
