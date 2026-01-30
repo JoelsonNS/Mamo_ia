95cbbbda80e3c5e6d53c542437ebfc09f2611c07
joelsonNS <joelson.n.s@hotmail.com>
2026-01-30 17:48:59 -0300

    Upgrade

--- js/5.1_edicao_laudo.js
+++ js/5.1_edicao_laudo.js
@@ -1,12 +1,16 @@
-
 const saveBtn = document.getElementById("saveBtn");
-
-
+const btnCancel = document.getElementById("btnCancel");
 
 // ============================
 // BOTÃO SALVAR
 // ============================
-
-saveBtn.addEventListener("click", () => {
+saveBtn.addEventListener('click', () => {
   alert("Configurações salvas com sucesso!");
+});
+
+// ============================
+// BOTÃO CANCELAR
+// ============================
+btnCancel.addEventListener('click', () => {
+  window.location.href = '5_pagina_resultado_analise_ia.html';
 });
