6ef2e0e31c9d4cd2aa6c4b4ad41ddc2f6f0de1e6
joelsonNS <joelson.n.s@hotmail.com>
2026-01-30 21:50:12 -0300

    melhorias contínuas

--- js/5.1_edicao_laudo.js
+++ js/5.1_edicao_laudo.js
@@ -1,11 +1,18 @@
 const saveBtn = document.getElementById("saveBtn");
 const btnCancel = document.getElementById("btnCancel");
+const textarea = document.querySelector(".laudo-editor textarea");
 
 // ============================
 // BOTÃO SALVAR
 // ============================
 saveBtn.addEventListener('click', () => {
-  alert("Configurações salvas com sucesso!");
+  const textoEditado = textarea.value;
+
+  // Salva o texto no localStorage
+  localStorage.setItem("resultadoLaudoIA", textoEditado);
+
+  // Redireciona para a página de resultado
+  window.location.href = '5_pagina_resultado_analise_ia.html';
 });
 
 // ============================
